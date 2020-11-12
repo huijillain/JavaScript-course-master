8fe2be511a71e368ce6e0921cb0d8b946fc3dad7
Sky <skyjillain@gmail.com>
2020-11-11 21:23:15 -0700

    functions, Arrays, continue & break

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -153,6 +153,285 @@ console.log(avgDolphins, avgKoalas);
 checkWinner(avgDolphins, avgKoalas);
 
 
+// Arrays
+const friend1 = 'Michael';
+const friend2 = 'Misty';
+const friend3 = 'Lucie';
 
+const friends = ['Michael', 'Misty', 'Lucie'];
+console.log(friends);
 
+const years = new Array(1991, 1984, 2008, 2020);
 
+console.log(friends[0]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]); // impressions in []
+
+// friends[0] = 'Colleen';
+console.log(friends);
+
+const firstName = 'Sky';
+const sky = [firstName, 'Jillain', 2020 - 1990, 'Software Developer', friends];
+console.log(sky);
+
+// Array exercise
+const calcAge5 = function (birthYear5) {
+    return 2020 - birthYear5;
+}
+const years2 = [1990, 1967, 2002, 2010, 2018];
+
+const age7 = calcAge5(years2[0]);
+const age8 = calcAge5(years2[1]);
+const age9 = calcAge5(years2[years2.length - 1]);
+const ages = [calcAge5(years2[0]), calcAge5(years2[1]), calcAge5(years2[years2.length - 1])];
+console.log(ages);
+
+// Array Operations(methods)
+const friends1 = ['Michael', 'Misty', 'Lucie'];
+//friends1.push('Sunny');
+const newLength = friends1.push('Sunny');  //add to end
+console.log(friends1);
+
+friends1.unshift('Amy');                   //add to begining
+console.log(friends1);
+
+friends1.pop();                           // remove the end
+const popped = friends1.pop();
+console.log(popped);
+console.log(friends1);
+
+friends1.shift();
+console.log(friends1);
+
+console.log(friends1.indexOf('Misty'));
+console.log(friends1.indexOf('Kevin'));
+
+friends1.push(23);
+console.log(friends1.includes('Misty'));
+console.log(friends1.includes('Kevin'));
+console.log(friends1.includes(23));
+
+if (friends1.includes('Misty')) {
+    console.log('You have a friend called Misty');
+}
+
+/* Coding Challenge #2
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+Your tasks:
+1. Write a function 'calcTip' that takes any bill value as an input and returns
+the corresponding tip, calculated based on the rules above (you can check out
+the code from first tip calculator challenge if you need to). Use the function
+type you like the most. Test the function using a bill value of 100
+2. And now let's use arrays! So create an array 'bills' containing the test data
+below
+3. Create an array 'tips' containing the tip value for each bill, calculated from
+the function you created before
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+Test data: 125, 555 and 44
+
+Hint: Remember that an array needs a value in each position, and that value can
+actually be the returned value of a function! So you can just call a function as array
+values (so don't store the tip values in separate variables first, but right in the new
+array)
+*/
+// const bills = [125, 555, 44];
+// const calcTip = bills >= 50 && bills <= 300 ? bills * 0.15 : bills * 0.2;
+
+// const total = bills + calcTip;
+// console.log(calcTip, total);
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+// const calcTip = bill => bill >= 50 && bill <= 300 ? bill *0.15 : bill * 0.2;
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, totals);
+
+//Objects
+const skyArray = [
+    'Hui',
+    'Jillain',
+    2020 - 1990,
+    'Software Developer',
+    ['Kevin', 'Chris', 'Bob']
+];
+
+const hui = { // 5 properties; in {} to define new objects
+    firstName: 'Sky',
+    lastName: 'Jillain',
+    age: 2020 - 1990,
+    job: 'Software Developer',
+    friends: ['Kevin', 'Chris', 'Bob'],
+};
+console.log(hui);
+
+//Dot vs. Bracket Notation
+console.log(hui.lastName);
+console.log(hui['job']);
+
+const nameKey = 'Name';
+console.log(hui['first' + nameKey]);
+console.log(hui['last' + nameKey]);
+
+const interestedIn = prompt('What do you want to know about hui? Choose between firstName, lastName, age, job and friends');
+console.log(hui[interestedIn]);
+
+if (hui[interestedIn]) {
+    console.log(hui[interestedIn]);
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job and friends')
+}
+hui.location = 'Calgary';
+hui['twitter'] = '@huijillain';
+console.log(hui);
+
+//challenge
+//"Hui has 3 friends, and her best friend is called Lucie"
+
+// console.log(hui.firstName, 'has', friends.length, 'friends, and her best friend is called', friends[2]);
+console.log(`${hui.firstName} has ${friends.length} friends, and his best friend is called ${friends[2]}`);
+
+//google search "mdn operator precedence", check table
+
+const hui2 = { // 5 properties; in {} to define new objects
+    firstName: 'Sky',
+    lastName: 'Jillain',
+    age: 2020 - 1990,
+    job: 'Software Developer',
+    friends: ['Kevin', 'Chris', 'Bob'],
+    hasDriversLicense: true,
+
+    calcAge8: function (birthYear4) {  //this is impression, property holds function value
+        return 2020 - birthYear4;
+    }
+};
+
+console.log(hui2.calcAge8(1996));
+// console.log(hui2['calcAge8'](1996));
+
+//this method
+const hui3 = {
+    firstName: 'Sky',
+    lastName: 'Jillain',
+    birthYear5: 1990,
+    job: 'Software Developer',
+    friends: ['Kevin', 'Chris', 'Bob'],
+    hasDriversLicense: false,
+
+    calcAge9: function () {
+        // console.log(this);
+        return 2020 - this.birthYear5;
+
+        // this.age = 2020 - this.birthYear5;
+        // return this.age;
+    },
+
+    getSummary: function () {
+        return `${this.firstName} is a ${this.calcAge9()}-year-old ${hui3.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+    }
+};
+
+console.log(hui3.calcAge9());
+
+//Challenge
+//"Sky is a 30-year-old Software Develooper, and he has a/no driver's license."
+console.log(hui3.getSummary()); //line330-333, then line 346
+
+/* Coding Challenge #3
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+implement the calculations! Remember: BMI = mass / height ** 2 = mass
+/ (height * height) (mass in kg and height in meter)
+Your tasks:
+1. For each of them, create an object with properties for their full name, mass, and
+height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+method on both objects). Store the BMI value to a property, and also return it
+from the method
+3. Log to the console who has the higher BMI, together with the full name and the
+respective BMI.
+Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+*/
+const mark = {
+    fullName: 'Mark Miller',
+    mess: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.mess / (this.height * this.height);
+        return this.bmi;
+    }
+};
+
+const john = {
+    fullName: 'John Smith',
+    mess: 92,
+    height: 1.95,
+
+    calcBMI: function () {
+        this.bmi = this.mess / (this.height * this.height);
+        return this.bmi;
+    }
+};
+mark.calcBMI();  // I missed this line
+john.calcBMI();
+console.log(mark.bmi, john.bmi);
+
+if (mark.bmi > john.bmi) {
+    console.log(`${mark.fullName}'s BMI (${mark.bmi} is higher than ${john.fullName}'s (${john.bmi})!`);
+} else if (john.bmi > mark.bmi) {
+    console.log(`${john.fullName}'s BMI (${john.bmi} is higher than ${mark.fullName}'s (${mark.bmi})!`);
+}
+
+// For Loop
+// console.log('Lifting weights repetition 1');
+// ...
+// console.log('Lifting weights repetition 5');
+
+for (let rep = 1; rep <= 10; rep++) {
+    // console.log('Lifting weights repetition ', rep);
+    console.log(`Lifting weights repetition ${rep} 🥋`)
+}
+
+const jason = [
+    'jay',
+    'Smith',
+    2020 - 1988,
+    'Instructor',
+    ['Kevin', 'Scott', 'Adam'],
+    true
+];
+const types = [];
+// console.log(jason[0])
+// ...
+// console.log(jason[4])
+for (let i = 0; i < jason.length; i++) {
+    // reading from jason array
+    console.log(jason[i], typeof jason[i]);
+
+    // Filling types array
+    // types[i] = typeof jason[i];
+    types.push(typeof jason[i]);
+}
+console.log(types);
+
+const years1 = [1991, 2007, 1969, 2020];
+const ages1 = [];
+for (let y = 0; y < years1.length; y++) {
+    ages1.push(2020 - years1[y]);
+}
+console.log(ages1);
+
+// continue and break
+console.log('--- ONLY STIRNGS ---')
+for (let z = 0; z < jason.length; z++) {
+    if (typeof jason[z] !== 'string') continue;
+    console.log(jason[z], typeof jason[z]);
+}
+
+console.log('--- BREAK WITH NUMBER ---')
+for (let z = 0; z < jason.length; z++) {
+    if (typeof jason[z] === 'number') break;
+    console.log(jason[z], typeof jason[z]);
+}
