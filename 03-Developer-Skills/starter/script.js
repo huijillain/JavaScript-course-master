eb74cc0c0601911995ef6bc955830ffe0c085d69
Sky <skyjillain@gmail.com>
2020-11-16 16:18:23 -0700

    add prettier for coding

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+//installing prettier, add .prettierrc to change "  to ' '
+const x = 23;
+if (x === 23) console.log(23);
 
+const calcAge = birthYear => 2020 - 1990;
