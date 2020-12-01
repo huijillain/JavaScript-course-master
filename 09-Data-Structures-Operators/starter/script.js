69b65b11f9505822749e3b6daccb795c820db54f
Sky <skyjillain@gmail.com>
2020-11-30 18:54:05 -0700

    spread & REST

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -255,3 +255,67 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = 'Ristorante roma';
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
+
+// 4, Rest patterns and parameters
+const restaurant1 = {
+  // this is hardcode data, it is useful to set default data like lines 117-118
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      // object in an object
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order1: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery1: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta1: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+};
+
+// SPREAD, because on RIGHT side of =
+const arr1 = [1, 2, ...[3, 4]];
+
+// REST, becasue on LEFT side of =
+// REST element must be the last element
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others); // answer 1 2 [3,4,5]
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+// Objects
+const 
