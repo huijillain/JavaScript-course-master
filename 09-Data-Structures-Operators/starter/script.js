ab6200fc2c92dc83af4cfc98d73302b0416f0e8e
Sky <skyjillain@gmail.com>
2020-12-03 21:07:33 -0700

    SETs in array

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -740,3 +740,42 @@ for (const [team, odd] of Object.entries(game2.odds)) {
 }
 
 // 4, in Github
+
+// SETs can never have any duplicate, need to be unique.
+// SETs are iterable.
+// No index in SET.
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+console.log(ordersSet);
+
+console.log(new Set('Sky'));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza')); // answer: true
+console.log(ordersSet.has('Bread')); // answer: false
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread'); // 2nd was ignored as SET need to be unique.
+ordersSet.delete('Risotto');
+// ordersSet.clear();
+
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+// const staffUnique = new Set(staff);
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+);
+
+console.log(new Set('skyjillain').size);
