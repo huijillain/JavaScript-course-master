e9695bc1b10820023f9a8876829516c20dbc48a6
Sky <skyjillain@gmail.com>
2020-12-04 20:01:56 -0700

    Maps: Iterations

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -816,3 +816,41 @@ console.log(rest.size);
 
 // console.log(rest.get([1, 2]));
 console.log(rest.get(arr2));
+
+// Maps: Iteration
+// Maps are also interable.
+// For loop is also for maps.
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz map
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number')
+    console.log(`Answer 
+  ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer'));
+const answer = 3; // for exercise: convert map to array below, so prompt does not pop out every time
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map back to array
+console.log([...question]);
+console.log([...question.entries()]);
+console.log(question.keys()); // can use [...] or without
+console.log(questioin.values());
