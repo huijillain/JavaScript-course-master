7d216ee90a1b82790d172253c0a301ecbeabec90
Sky <skyjillain@gmail.com>
2020-12-11 16:57:01 -0700

    Default parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const bookings = [];
+//  // ES5 way of adding parameters
+// const createBooking = function (flightNum, numPassengers, price) {
+//     numPassengers = numPassengers || 1;
+//     price = price || 199;
+
+// ES6
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers // afater line 12!!!
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('HL123');
+createBooking('HL123', 2, 800);
+createBooking('HL123', 3);
+createBooking('HL123', undefined, 985);
